9b65f7dacb5684d49129e4ac3ea2ea328cca70d0
mpettyjo <mpettjo@ualberta.ca>
2023-03-20 14:16:49 +1100

    fix doc-sting

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,5 +1,6 @@
 '''
-This program simulates the distribution of foreground stars toward the Andromeda Galaxy.
+This program simulates the distribution of foreground stars
+toward the Andromeda Galaxy in ra/dec.
 '''
 
 # import python packages for use in this program
@@ -8,30 +9,31 @@ import random # uniform
 
 # Determine Andromeda location in ra/dec degrees
 # from wikipedia
-RA = '00:42:44.3'
-DEC = '41:16:09'
+ra = '00:42:44.3'
+dec = '41:16:09'
 
 # convert to decimal degrees
-d, m, s = DEC.split(':')
-DEC = int(d)+int(m)/60+float(s)/3600
+D, M, S = dec.split(':')
+dec = int(D)+int(M)/60+float(S)/3600
 
-h, m, s = RA.split(':')
-RA = 15*(int(h)+int(m)/60+float(s)/3600)
-RA = RA/math.cos(DEC*math.pi/180)
+H, M, S = ra.split(':')
+ra = 15*(int(H)+int(M)/60+float(S)/3600)
+ra = ra/math.cos(dec*math.pi/180)
 
 NSRC = 1_000_000
 
 # make 1000 stars within 1 degree of Andromeda
-RAs = []
-DECs = []
+ras = []
+decs = []
 for i in range(NSRC):
-    RAs.append(RA + random.uniform(-1, 1))
-    DECs.append(DEC + random.uniform(-1, 1))
+    ras.append(ra + random.uniform(-1, 1))
+    decs.append(dec + random.uniform(-1, 1))
 
 
 # now write these to a csv file for use by my other program
 f = open('/Users/mpettyjo/Documents/ADACS Workshop/data/processed/catalog.csv', 'w')
 print("id,ra,dec", file=f)
 for i in range(NSRC):
-    print("{0:07d}, {1:12f}, {2:12f}".format(i, RAs[i], DECs[i]), file=f)
+    print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
 f.close()
+
